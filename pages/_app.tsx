588ec967c46ce5196245e19c98e59b4ec63a74aa
Makoto Yamada <woocie.rugby@gmail.com>
2021-09-09 20:50:44 +0900

    use styled-component instead of pure css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import type { ReactElement } from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+const theme = {};
 
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>{" "}
+    </>
+  );
 };
 export default MyApp;
